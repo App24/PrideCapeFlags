2e89d8253973d8a8358dd37f653205b9650c9925
Ricardo <rmmx03@gmail.com>
2026-06-02 11:00:31 +0100

    Removed unused codec

--- src/main/java/org/github/app24/pridecapeflags/CapeTexture.java
+++ src/main/java/org/github/app24/pridecapeflags/CapeTexture.java
@@ -8,11 +8,6 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.Identifier;
 
 public record CapeTexture(Identifier id, Identifier texturePath) implements ClientAsset.Texture {
-    public static final Codec<ClientAsset.ResourceTexture> CODEC = Identifier.CODEC.xmap(ClientAsset.ResourceTexture::new, ClientAsset.ResourceTexture::id);
-    public static final MapCodec<ClientAsset.ResourceTexture> DEFAULT_FIELD_CODEC = CODEC.fieldOf("asset_id");
-    public static final StreamCodec<ByteBuf, ClientAsset.ResourceTexture> STREAM_CODEC = Identifier.STREAM_CODEC
-            .map(ClientAsset.ResourceTexture::new, ClientAsset.ResourceTexture::id);
-
     public CapeTexture(Identifier texture) {
         this(texture, texture.withPath(path -> "flags/" + path + ".png"));
     }
