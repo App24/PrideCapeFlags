3fe9da439a74cba797295a3dc4be2b49b33811bd
Ricardo <rmmx03@gmail.com>
2026-06-23 18:59:27 +0100

    Pride title screen

---
+++ src/main/java/org/github/app24/pridecapeflags/mixin/LogoRendererMixin.java
@@ -0,0 +1,44 @@
+package org.github.app24.pridecapeflags.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.LogoRenderer;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.Identifier;
+import org.github.app24.pridecapeflags.Config;
+import org.github.app24.pridecapeflags.PrideCapeFlagsMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LogoRenderer.class)
+public class LogoRendererMixin {
+    @Unique
+    private static final Identifier PRIDE_LOGO = Identifier.fromNamespaceAndPath(PrideCapeFlagsMod.MODID, "textures/gui/title/pridecraft.png");
+    @Unique
+    private static final Identifier MINECRAFT_EDITION = Identifier.fromNamespaceAndPath(PrideCapeFlagsMod.MODID,"textures/gui/title/edition.png");
+
+    @WrapOperation(method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIFFIIIII)V", ordinal = 0))
+    private void renderPrideCraftLogo(GuiGraphics instance, RenderPipeline renderPipeline, Identifier texture, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight, int color, Operation<Void> original){
+        if(!PrideCapeFlagsMod.IS_PRIDE || !Config.SHOW_PRIDE_TITLE.get()){
+            original.call(instance, renderPipeline, texture, x, y, u, v, width, height, textureWidth, textureHeight, color);
+            return;
+        }
+        instance.blit(
+                RenderPipelines.GUI_TEXTURED, PRIDE_LOGO, x, y, u,v,width,height,textureWidth,textureHeight,color
+        );
+    }
+
+    @WrapOperation(method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIFFIIIII)V", ordinal = 1))
+    private void renderPrideEdition(GuiGraphics instance, RenderPipeline renderPipeline, Identifier texture, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight, int color, Operation<Void> original){
+        if(!PrideCapeFlagsMod.IS_PRIDE || !Config.SHOW_PRIDE_TITLE.get()){
+            original.call(instance, renderPipeline, texture, x, y, u, v, width, height, textureWidth, textureHeight, color);
+            return;
+        }
+        instance.blit(
+                RenderPipelines.GUI_TEXTURED, MINECRAFT_EDITION, x, y, u,v,width,height,textureWidth,textureHeight,color
+        );
+    }
+}
